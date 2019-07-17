33e00ccc350ccffa77b073712a5cb7d02e46a803
Md. Obydullah <obydul@makpie.com>
2019-07-17 11:35:38 +0600

    Update LaraSkrill Config File

--- config/laraskrill.php
+++ config/laraskrill.php
@@ -10,8 +10,9 @@ return [
     'api_password' => 'MD5 API/MQI password', // required for refund option only.
     'return_url' => 'https://laravel.dev/payment-completed',
     'cancel_url' => 'https://laravel.dev/payment-cancelled',
-    'status_url' => 'your-email@example.com', // url or email
+    'status_url' => 'obydulcse@gmail.com', // url or email
 //    'status_url' => 'http://laravel.dev/ipn', // url or email
+    'status_url2' => 'mdobydullahcse@gmail.com', // url or email
     'refund_status_url' => 'IPN URL or Email', // url or email
     'logo_url' => 'https://i.imgur.com/BYBiIZX.png',
 ];
